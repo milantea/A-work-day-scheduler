75cffb1a1c83434e6d4f55e388f56d08e9dbcc20
Milante Adams <milanteadams@Milantes-MBP.attlocal.net>
2022-07-07 14:03:37 -0400

    my first commit

---
+++ Module_5_Challenge/script.js
@@ -0,0 +1,17 @@
+let hourlyTasks = document.querySelector("tasks");
+let saveEntries = document.querySelector("#saveBtn");
+let saveTimeEntries = localStorage.getItem("saveTimeEntries");
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+document.querySelector("#currentDay").textContent =
+  new Date().toLocaleDateString(options);
+
+const saveTasks = (e) => {
+  e.preventDefault();
+  console.log(e);
+  console.log(this);
+};
